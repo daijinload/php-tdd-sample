3ac360b79aeb98ca6a3b6fbe0731097010184341
unknown <01015865@ancor2.r.recruit.co.jp>
2016-05-16 17:24:23 +0900

    second commit

--- src/Hoge.php
+++ src/Hoge.php
@@ -1,5 +1,13 @@
 <?php
+
+namespace Daijinload\Phptest;
+
 class Hoge {
+
+	function __construct() {
+		print "In Hoge constructor\n";
+	}
+
 	public $var = 'hello world!!';
 
 	public function say() {
